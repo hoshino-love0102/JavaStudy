63d781346f9f08259f39bd5c4ae02994dff6fdff
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-06-05 17:36:44 +0900

    Student.java: 이름과 점수를 가진 학생 클래스 구현

--- javastudy20250605/src/Student.java
+++ javastudy20250605/src/Student.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + score + "점)";
+    }
 }
