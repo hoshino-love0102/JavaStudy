8335f75c5d4461c6f4b033ec900a3ee1b5d97c9b
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-14 19:34:44 +0900

    Create Main class to test Animal subclasses and Walkable interface

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog("Buddy");
+        Animal cat = new Cat("Kitty");
+
+        dog.makeSound();
+        cat.makeSound();
+
+        ((Walkable) dog).walk();
+        ((Walkable) cat).walk();
+
+        dog.sleep();
+        cat.sleep();
+    }
 }
